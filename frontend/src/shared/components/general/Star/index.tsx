b0ac55f689831ff1173c4de858f3f4e55a1a289c
Rodrigo <rodrigo@gtacsolutions.com>
2022-05-06 00:44:39 -0300

    feat(components): add onPress to Star

--- frontend/src/shared/components/general/Star/index.tsx
+++ frontend/src/shared/components/general/Star/index.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 
 // Components
 import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 
 // Hooks
 import { useTheme } from 'store/slices/themeSlice';
 
 interface Props {
   isActive: boolean;
+  onPress?: () => void;
 }
 
-export const Star = ({ isActive }: Props) => {
+export const Star = ({ isActive, onPress }: Props) => {
   const { colors } = useTheme();
 
   return (
-    <AntDesign
-      size={24}
-      name={isActive ? 'star' : 'staro'}
-      color={isActive ? colors.starColor : colors.textColor}
-    />
+    <TouchableOpacity onPress={onPress}>
+      <AntDesign
+        size={24}
+        name={isActive ? 'star' : 'staro'}
+        color={isActive ? colors.starColor : colors.textColor}
+      />
+    </TouchableOpacity>
   );
 };
