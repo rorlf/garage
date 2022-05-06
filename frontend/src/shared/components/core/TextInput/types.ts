98e9a303c079003d61f4c32c1be4e0d5fdea6494
Rodrigo <rodrigo@gtacsolutions.com>
2022-05-06 12:32:45 -0300

    feat(project): remove unused files

--- frontend/src/shared/components/core/TextInput/types.ts
+++
@@ -1,6 +0,0 @@
-import { RefObject } from 'react';
-import { TextInput, TextInputProps } from 'react-native';
-
-export interface CustomTextInputProps extends TextInputProps {
-  innerRef: RefObject<TextInput>;
-}
